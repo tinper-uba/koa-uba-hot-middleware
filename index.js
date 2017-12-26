34aede3863b0b9c1c3f9a0702118e10f1ed7ae41
kvkens <kvkens@qq.com>
2017-12-26 19:11:18 +0800

    bugfix:解决无法404的问题。

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * koa2 hot middleware for uba-server
  * Version  : v1.0.0
  * Author   : Kvkens(yueming@yonyou.com)
- * Date     : 2017-12-22 12:25:24
+ * Date     : 2017-12-26 18:52:18
  */
 
 const webpackHot = require('webpack-hot-middleware');
@@ -17,10 +17,10 @@ const hotMiddleware = (compiler, opts) => {
   const middleware = webpackHot(compiler, opts);
   return async(ctx, next) => {
     let stream = new PassThrough();
-    ctx.body = stream;
     await middleware(ctx.req, {
       write: stream.write.bind(stream),
       writeHead: (status, headers) => {
+        ctx.body = stream;
         ctx.status = status;
         ctx.set(headers);
       }
